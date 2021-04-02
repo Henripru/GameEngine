e6cf7fcc41a5008715380619a14bfee87cd1cacc
Henri Prudhomme <henripru99@gmail.com>
2021-04-02 14:35:43 -0400

    Add a hello world with QGraphicsView

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QtGui>
+#include <QGraphicsView>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    //MainWindow w;
+    //w.show();
+
+    QGraphicsView w;
+    QGraphicsScene *scene = new QGraphicsScene(0, 0, 300, 300, &w);
+    scene->setBackgroundBrush(Qt::blue);
+    scene->addText("Hello World!");
+
+    w.setScene(scene);
+
     w.show();
+
     return a.exec();
 }
